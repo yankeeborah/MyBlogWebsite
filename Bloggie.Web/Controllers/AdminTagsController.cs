781c24542d7d633c9a49fb543374a1d28ea05cb4
Biswa_ASUS <Biswajitborah96@gmail.com>
2024-08-05 12:45:35 +0530

    CRUD

---
+++ Bloggie.Web/Controllers/AdminTagsController.cs
@@ -0,0 +1,39 @@
+﻿using Bloggie.Web.Data;
+using Bloggie.Web.Models.Domain;
+using Bloggie.Web.Models.ViewModels;
+using Microsoft.AspNetCore.Mvc;
+
+namespace Bloggie.Web.Controllers
+{
+    public class AdminTagsController : Controller
+    {
+        private readonly BloggieDbContext bloggieDbContext;
+
+        //to use it with the get and post as we cannot use the object of Dbcontect directly bacuse of constructor scope
+        public AdminTagsController(BloggieDbContext bloggieDbContext)
+        {
+            this.bloggieDbContext = bloggieDbContext;
+        }
+        [HttpGet]
+        public IActionResult Add()
+        {
+            return View();
+        }
+
+        [HttpPost]
+        [ActionName("Add")]
+        public IActionResult SubmitTag(AddTagRequest addTagRequest)
+        {
+            //Mapping AddTagRequest to Tag domain Model
+            var tag = new Tag()
+            {
+                Name = addTagRequest.Name,
+                DisplayName = addTagRequest.DisplayName,    
+
+            };
+            bloggieDbContext.Tags.Add(tag);
+            bloggieDbContext.SaveChanges();
+            return View("Add");
+        }
+    }
+}

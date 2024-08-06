33c020bb84807c9f91cda5dfc78dd93aac2333ab
Biswa_ASUS <Biswajitborah96@gmail.com>
2024-08-07 01:02:38 +0530

    Asynch Await

--- Bloggie.Web/Controllers/AdminTagsController.cs
+++ Bloggie.Web/Controllers/AdminTagsController.cs
@@ -2,6 +2,7 @@
 using Bloggie.Web.Models.Domain;
 using Bloggie.Web.Models.ViewModels;
 using Microsoft.AspNetCore.Mvc;
+using Microsoft.EntityFrameworkCore;
 
 namespace Bloggie.Web.Controllers
 {
@@ -15,14 +16,14 @@ namespace Bloggie.Web.Controllers
             this.bloggieDbContext = bloggieDbContext;
         }
         [HttpGet]
-        public IActionResult Add()
+        public IActionResult Add()      //no need to make async as not doing any I/O
         {
             return View();
         }
 
         [HttpPost]
         [ActionName("Add")]
-        public IActionResult SubmitTag(AddTagRequest addTagRequest)
+        public async Task<IActionResult> Add(AddTagRequest addTagRequest)
         {
             //Mapping AddTagRequest to Tag domain Model
             var tag = new Tag()
@@ -31,28 +32,28 @@ namespace Bloggie.Web.Controllers
                 DisplayName = addTagRequest.DisplayName,    
 
             };
-            bloggieDbContext.Tags.Add(tag);
-            bloggieDbContext.SaveChanges();
+            await bloggieDbContext.Tags.AddAsync(tag);
+            await bloggieDbContext.SaveChangesAsync();
             return RedirectToAction("List");
         }
 
         [HttpGet]
         [ActionName("List")]
-        public IActionResult List()
+        public async Task<IActionResult> List()
         {
             //use DbContext to read the tags
-            var tags = bloggieDbContext.Tags.ToList();
+            var tags = await bloggieDbContext.Tags.ToListAsync();
 
             return View(tags);
         }
         [HttpGet]
-        public IActionResult Edit(Guid id)  //name ofparameter has to match with the route attribute in view
+        public async Task<IActionResult> Edit(Guid id)  //name ofparameter has to match with the route attribute in view
         {
             //1st method
             //var tag = bloggieDbContext.Tags.Find(id);
 
             //2nd method
-            var tag = bloggieDbContext.Tags.FirstOrDefault(x=>x.Id== id);
+            var tag = await bloggieDbContext.Tags.FirstOrDefaultAsync(x=>x.Id== id);
             if (tag != null) 
             {
                 var editTagRequest = new EditTagRequest()
@@ -67,7 +68,7 @@ namespace Bloggie.Web.Controllers
         }
 
         [HttpPost]
-        public IActionResult Edit(EditTagRequest editTagRequest)
+        public async Task<IActionResult> Edit(EditTagRequest editTagRequest)
         {
             var tag = new Tag()
             {
@@ -75,26 +76,26 @@ namespace Bloggie.Web.Controllers
                 Name = editTagRequest.Name,
                 DisplayName = editTagRequest.DisplayName
             };
-            var existingTag = bloggieDbContext.Tags.Find(tag.Id);
+            var existingTag = await bloggieDbContext.Tags.FindAsync(tag.Id);
             if (existingTag != null) 
             { 
                 existingTag.Name = tag.Name;    
                 existingTag.DisplayName = tag.DisplayName;
 
-                bloggieDbContext.SaveChanges();//save changes
+                await bloggieDbContext.SaveChangesAsync();//save changes
                 return RedirectToAction("List");//show successful notification
             }
             return RedirectToAction("Edit",new {id=editTagRequest.Id});//show failure notification
         }
 
-        public IActionResult Delete(EditTagRequest editTagRequest)
+        public async Task<IActionResult> Delete(EditTagRequest editTagRequest)
         {
-            var tag = bloggieDbContext.Tags.Find(editTagRequest.Id);
+            var tag = await bloggieDbContext.Tags.FindAsync(editTagRequest.Id);
             if (tag != null)
             {
                 bloggieDbContext.Tags.Remove(tag);//Remove from the Tags table
 
-                bloggieDbContext.SaveChanges();//save changes
+                await bloggieDbContext.SaveChangesAsync();//save changes
                 return RedirectToAction("List");//show successful notification
             }
             //show an error notification
